d4a999ab07f466aa443b35b7c254b0ca65e5fea6
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-09 12:47:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function calculateGrade(percentage) {
   //your code here
+	let markPercentage = Math.round(percentage);
+
+	if(markPercentage >= 90)
+	{
+		return 'A';
+	}
+	else if(markPercentage >= 80 && markPercentage<= 89)
+	{
+		return 'B';
+	}
+	else if(markPercentage >= 70 && markPercentage<= 79)
+	{
+		return 'C';
+	}
+	else if(markPercentage >= 60 && markPercentage<= 69)
+	{
+		return 'D';
+	}
+	else
+	{ 
+		return 'F';
+	}
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
